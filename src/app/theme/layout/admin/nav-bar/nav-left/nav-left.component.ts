4e2569d27403b7adca74a0e77c1714d404f91366
mohsinalimomin <mohsinanvar123@gmail.com>
2025-06-19 13:36:10 +0530

    search location functionality

--- src/app/theme/layout/admin/nav-bar/nav-left/nav-left.component.ts
+++ src/app/theme/layout/admin/nav-bar/nav-left/nav-left.component.ts
@@ -1,5 +1,5 @@
 // angular import
-import { Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 // project import
 import { SharedModule } from 'src/app/theme/shared/shared.module';
@@ -9,25 +9,30 @@ import { NavSearchComponent } from './nav-search/nav-search.component';
 import screenfull from 'screenfull';
 import { BaseService } from 'src/app/services/base.service';
 import { Router, RouterModule } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
-  selector: 'app-nav-left',
-  imports: [SharedModule, NavSearchComponent, RouterModule],
-  templateUrl: './nav-left.component.html',
-  styleUrls: ['./nav-left.component.scss']
+	selector: 'app-nav-left',
+	imports: [SharedModule, NavSearchComponent, RouterModule],
+	templateUrl: './nav-left.component.html',
+	styleUrls: ['./nav-left.component.scss']
 })
 export class NavLeftComponent implements OnInit, OnDestroy {
- 
+
 	screenFull = true;
 
 	//categories item array
 	categories: any[] = [];
 	//to denote active
 	activeItem: string = '';
+	searchResult: any[] = [];
+	searchText: string = '';
+	selectedRegionData: any = null;
 
-	constructor(private baseService: BaseService, private router:Router) {}
+	constructor(private baseService: BaseService, private router: Router, private dataService: DataService) { }
 	// life cycle hook
 	ngOnInit() {
+		this.dataService.regionChanged = true;
 		if (screenfull.isEnabled) {
 			this.screenFull = screenfull.isFullscreen; // Initialize based on current fullscreen state
 			screenfull.on('change', () => {
@@ -47,60 +52,80 @@ export class NavLeftComponent implements OnInit, OnDestroy {
 
 
 	toggleFullscreen() {
-	if (screenfull.isEnabled) {
-		screenfull.toggle().then(() => {
-			this.screenFull = screenfull.isFullscreen;
-		});
-	}
-	}
-
-
-	getRegionBySearch(event: KeyboardEvent){
-	const input = event.target as HTMLInputElement;
-	let regionName = input.value.trim();
-	const onlyAlphabets = /^[a-zA-Z]$/.test(event.key);
-		if(event.key.length == 1 && onlyAlphabets && regionName.length >= 3){
-			
-			console.log("Keyborad event : "+event.key);
-			console.log("input data : "+regionName);
-
-			let maxrecord = 10;
-			this.baseService.GET("https://localhost:7282/api/ServiceAreaMapping/GetAreaBySearch?name="+ regionName+"&maxrecord="+maxrecord)
-			.subscribe(response => {
-				console.log("Get area by search : "+JSON.stringify(response));
-			})
+		if (screenfull.isEnabled) {
+			screenfull.toggle().then(() => {
+				this.screenFull = screenfull.isFullscreen;
+			});
 		}
 	}
 
-	loadCategories(){
+
+	getRegionBySearch(event: KeyboardEvent) {
+		const input = event.target as HTMLInputElement;
+		let regionName = input.value.trim();
+		const onlyAlphabets = /^[a-zA-Z]$/.test(event.key);
+		if (regionName.length === 0) {
+			this.searchResult = [];
+			this.selectedRegionData = null;
+			return;
+		}
+		if (event.key.length == 1 && onlyAlphabets && regionName.length >= 2) {
+
+			console.log("Keyborad event : " + event.key);
+			console.log("input data : " + regionName);
+
+			let maxrecord = 10;
+			this.baseService.GET<any>("https://localhost:7282/api/ServiceAreaMapping/GetAreaBySearch?name=" + regionName + "&maxrecord=" + maxrecord)
+				.subscribe(response => {
+					console.log("Get area by search : " + JSON.stringify(response));
+					this.searchResult = response.data;
+					console.log("search result : " + this.searchResult);
+				})
+		}
+	}
+
+	selectedRegion(region: any) {
+		console.log("this is selected region" + region.id);
+		this.selectedRegionData = region;
+		this.searchText = region.name + " " + region.parent;
+		this.searchResult = [];
+	}
+
+	loadCategories() {
 		this.baseService.GET<any>("https://localhost:7282/api/Category").subscribe(response => {
-			console.log("All categories : "+JSON.stringify(response.data).toString());
+			console.log("All categories : " + JSON.stringify(response.data).toString());
 			this.categories = response.data;
 		})
 		this.activeItem = this.categories[0];
 	}
 
-	navigateToSubCategory(categoryId:number){
+	navigateToSubCategory(categoryId: number) {
 		console.log(categoryId);
-		if(categoryId == 1){
+		if (categoryId == 1) {
 			this.router.navigate(['home'],
-				{queryParams : {
-					'categoryId':categoryId
-				}}
+				{
+					queryParams: {
+						'categoryId': categoryId
+					}
+				}
 			);
 		}
-		else if(categoryId == 2){
+		else if (categoryId == 2) {
 			this.router.navigate(['beauty'],
-				{queryParams : {
-					'categoryId':categoryId
-				}}
+				{
+					queryParams: {
+						'categoryId': categoryId
+					}
+				}
 			);
 		}
-		else if(categoryId == 3){
+		else if (categoryId == 3) {
 			this.router.navigate(['native'],
-				{queryParams : {
-					'categoryId':categoryId
-				}}
+				{
+					queryParams: {
+						'categoryId': categoryId
+					}
+				}
 			);
 		}
 	}
